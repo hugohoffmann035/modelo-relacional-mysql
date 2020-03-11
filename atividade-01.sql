b5dfa5c3fab7f9394c0868b2a3f34f0bbbb30e66
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2020-03-11 17:31:06 -0300

    v2.0

--- atividade-01.sql
+++ atividade-01.sql
@@ -17,39 +17,33 @@ drop table if exists setores_empregados;
 drop table if exists projeto_empregados;
 
 -- > Cria Tabelas
+CREATE TABLE IF NOT EXISTS projetos (
+	codigo INT(11) NOT NULL AUTO_INCREMENT,
+	abreviatura VARCHAR(45) NOT NULL,
+	descricao TEXT NOT NULL,
+    INDEX IDX_projetos_codigo (codigo),
+	PRIMARY KEY (codigo)
+) DEFAULT CHARSET UTF8 
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS empregados (
+	codigo INT(11) NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(45) NOT NULL,
+    INDEX IDX_empregados_codigo (codigo),
+	PRIMARY KEY (codigo)
+) DEFAULT CHARSET UTF8
+ENGINE = InnoDB;
+
 CREATE TABLE IF NOT EXISTS projeto_empregados (
 	codigo_projeto INT(11) NOT NULL,
 	codigo_empregado INT(11) NOT NULL,
     INDEX IDX_projeto_empregados_codigo_projeto (codigo_projeto),
     INDEX IDX_projeto_empregados_codigo_empregado (codigo_empregado),
-	PRIMARY KEY (codigo_projeto, codigo_empregado)
-) DEFAULT CHARSET UTF8
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS cargos_empregados (
-	codigo_cargo INT(11) NOT NULL,
-    codigo_empregado INT(11) NOT NULL,
-    INDEX IDX_cargos_empregados_codigo_cargo (codigo_cargo),
-    INDEX IDX_cargos_empregados_codigo_empregado (codigo_empregado),
-    PRIMARY KEY (codigo_cargo, codigo_empregado)
-) DEFAULT CHARSET UTF8 ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS setores_empregados (
-	codigo_setores INT(11) NOT NULL,
-    codigo_empregado INT(11) NOT NULL,
-    INDEX IDX_setores_empregados_codigo_setores (codigo_setores),
-    INDEX IDX_setores_empregados_codigo_empregado (codigo_empregado),
-    PRIMARY KEY (codigo_setores, codigo_empregado)
-) DEFAULT CHARSET UTF8 ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS setores (
-	codigo INT(11) NOT NULL AUTO_INCREMENT,
-	codigo_projeto INT(11) NOT NULL,
-	descricao TEXT NOT NULL,
-    INDEX IDX_setores_codigo (codigo),
-    INDEX IDX_setores_codigo_projeto (codigo_projeto),
-	PRIMARY KEY (codigo),
-    CONSTRAINT FK_setores_codigo FOREIGN KEY IDX_setores_codigo (codigo) REFERENCES setores_empregados (codigo_setores)
+	PRIMARY KEY (codigo_projeto, codigo_empregado),
+    CONSTRAINT FK_projeto_empregados_codigo_projeto FOREIGN KEY IDX_projeto_empregados_codigo_projeto (codigo_projeto) REFERENCES projetos (codigo)
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION,
+    CONSTRAINT FK_projeto_empregados_codigo_empregado FOREIGN KEY IDX_projeto_empregados_codigo_empregado (codigo_empregado) REFERENCES empregados (codigo)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION
 ) DEFAULT CHARSET UTF8
@@ -63,37 +57,49 @@ CREATE TABLE IF NOT EXISTS cargos (
     INDEX IDX_cargos_codigo (codigo),
     INDEX IDX_cargos_codigo_projeto (codigo_projeto),
     PRIMARY KEY (codigo),
-    CONSTRAINT FK_cargos_empregados_codigo FOREIGN KEY IDX_cargos_codigo (codigo) REFERENCES cargos_empregados (codigo_cargo)
+    CONSTRAINT FK_cargos_codigo_projeto FOREIGN KEY IDX_cargos_codigo_projeto (codigo) REFERENCES projetos (codigo)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION
 ) DEFAULT CHARSET UTF8
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS empregados (
-	codigo INT(11) NOT NULL AUTO_INCREMENT,
-	nome VARCHAR(45) NOT NULL,
-    INDEX IDX_empregados_codigo (codigo),
-	PRIMARY KEY (codigo),
-    CONSTRAINT FK_projeto_empregados_codigo FOREIGN KEY IDX_empregados_codigo (codigo) REFERENCES projeto_empregados (codigo_empregado)
+CREATE TABLE IF NOT EXISTS cargos_empregados (
+	codigo_cargo INT(11) NOT NULL,
+    codigo_empregado INT(11) NOT NULL,
+    INDEX IDX_cargos_empregados_codigo_cargo (codigo_cargo),
+    INDEX IDX_cargos_empregados_codigo_empregado (codigo_empregado),
+    PRIMARY KEY (codigo_cargo, codigo_empregado),
+    CONSTRAINT FK_cargos_empregados_codigo_cargo FOREIGN KEY IDX_cargos_empregados_codigo_cargo (codigo_cargo) REFERENCES cargos (codigo)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION,
-    CONSTRAINT FK_cargos_empregados_codigo_empregado FOREIGN KEY IDX_empregados_codigo (codigo) REFERENCES cargos_empregados (codigo_empregado)
+    CONSTRAINT FK_cargos_empregados_codigo_empregado FOREIGN KEY IDX_cargos_empregados_codigo_empregado (codigo_empregado) REFERENCES empregados (codigo)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION
-) DEFAULT CHARSET UTF8
-ENGINE = InnoDB;
+) DEFAULT CHARSET UTF8 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS projetos (
+CREATE TABLE IF NOT EXISTS setores (
 	codigo INT(11) NOT NULL AUTO_INCREMENT,
-	abreviatura VARCHAR(45) NOT NULL,
+	codigo_projeto INT(11) NOT NULL,
 	descricao TEXT NOT NULL,
-    INDEX IDX_projetos_codigo (codigo),
+    INDEX IDX_setores_codigo (codigo),
+    INDEX IDX_setores_codigo_projeto (codigo_projeto),
 	PRIMARY KEY (codigo),
-    CONSTRAINT FK_setores_codigo_projeto FOREIGN KEY IDX_projetos_codigo (codigo) REFERENCES setores (codigo_projeto)
-    ON DELETE RESTRICT
-    ON UPDATE NO ACTION,
-    CONSTRAINT FK_cargos_codigo_projeto FOREIGN KEY IDX_projetos_codigo (codigo) REFERENCES cargos (codigo_projeto)
+    CONSTRAINT FK_setores_codigo_projeto FOREIGN KEY IDX_setores_codigo_projeto (codigo) REFERENCES projetos (codigo)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION
-) DEFAULT CHARSET UTF8 
+) DEFAULT CHARSET UTF8
 ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS setores_empregados (
+	codigo_setores INT(11) NOT NULL,
+    codigo_empregado INT(11) NOT NULL,
+    INDEX IDX_setores_empregados_codigo_setores (codigo_setores),
+    INDEX IDX_setores_empregados_codigo_empregado (codigo_empregado),
+    PRIMARY KEY (codigo_setores, codigo_empregado),
+    CONSTRAINT FK_setores_empregados_codigo_setores FOREIGN KEY IDX_setores_empregados_codigo_setores (codigo_setores) REFERENCES setores (codigo)
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION,
+    CONSTRAINT FK_setores_empregados_codigo_empregado FOREIGN KEY IDX_setores_empregados_codigo_empregado (codigo_empregado) REFERENCES empregados (codigo)
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION
+) DEFAULT CHARSET UTF8 ENGINE = InnoDB;
